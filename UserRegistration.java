9a360761ffe5f14fdef02160c9e0b54ed1685bd3
IamSanketMore <sanketmore.999.m@gmail.com>
2021-02-20 23:57:01 +0530

    UC:- Check First name First is Capital and more than 3 characters

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,7 +1,34 @@
+import java.util.*;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 public class UserRegistration
+
 {
-	public static void main(String args[])
+
+	public void checkFirstName(String firstName)
 	{
-		System.out.println("Welcome to User Registration Problems using Regex");
+		//------------Regex------------------
+		String regex = "[A-Z]{1}[a-z]{2,}";
+
+		Pattern patternChecker = Pattern.compile(regex);
+		Matcher matchChecker = patternChecker.matcher(firstName);
+
+		//-------------Check valid or Not-----
+		if(matchChecker.matches())
+			System.out.println("Valid First Name");
+		else
+			System.out.println("InValid First Name");
+	}
+	public static void main(String args [])
+	{
+		Scanner scanReader = new Scanner(System.in);
+
+		UserRegistration newName = new UserRegistration();
+
+		System.out.println("Enter First Name:-");
+		String FirstName = scanReader.nextLine();
+		newName.checkFirstName(FirstName);
+
+		scanReader.close();
 	}
 }
