9a3567341692dcb08627358f4a6e80a50a40557c
IamSanketMore <sanketmore.999.m@gmail.com>
2021-02-21 00:14:14 +0530

    UC3:- Check Email is correct way or not

--- UserRegistration.java
+++ UserRegistration.java
@@ -4,31 +4,29 @@ import java.util.regex.Matcher;
 
 public class UserRegistration
 {
-
-	public void checkLastName(String lastName)
+	public void checkEmail(String emailId)
 	{
 		//------------Regex------------------
-		String regex = "[A-Z]{1}[a-z]{2,}";
-
+		String regex = "^[a-z0-9]{3,}+([_+-.][a-z0-9]+)?(@[a-z0-9]+)([.][a-z]{2,4})([.][a-z]{2})?$";
 		Pattern patternChecker = Pattern.compile(regex);
-		Matcher matchChecker = patternChecker.matcher(lastName);
+		Matcher matchChecker = patternChecker.matcher(emailId);
 
 		//-------------Check valid or Not-----
 		if(matchChecker.matches())
-			System.out.println("Valid Last Name");
+			System.out.println("Valid Email");
 		else
-			System.out.println("InValid Last Name");
+			System.out.println("InValid Email");
 	}
 	public static void main(String args [])
 	{
 		Scanner scanReader = new Scanner(System.in);
 
 		UserRegistration newName = new UserRegistration();
-
-		System.out.println("Enter First Name:-");
-		String LastName = scanReader.nextLine();
-		newName.checkLastName(LastName);
+		System.out.println("Enter Email:-");
+		String email = scanReader.nextLine();
+		newName.checkEmail(email);
 
 		scanReader.close();
 	}
 }
+
